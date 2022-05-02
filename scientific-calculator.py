ce8a8bc3b1de1df7ee499da1b9a6ee0cdb17ee1d
GedeonTS <gedeontshobohwa1@gmail.com>
2022-05-02 20:52:03 +0200

    calculator

---
+++ scientific-calculator.py
@@ -0,0 +1,15 @@
+import math
+def addition(x,y):
+    return x+y
+def substract(x,y):
+    return x-y
+def multiplication(x,y):
+    return x*y
+def modulo(x,y):
+    return x%y
+def square(x):
+    return x*x
+def root(x):    
+    return sqrt(x)
+def cosine(x):
+    math.cos()
